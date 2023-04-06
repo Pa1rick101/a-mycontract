8ec3909cb78e9d1f81364d509d117e7475f99c27
Pa1rick101 <p.suehrer@gmail.com>
2023-04-06 15:22:50 +0200

    changed testing lib

--- test/MyCar.t.sol
+++ test/MyCar.t.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "ds-test/test.sol";
 import "../src/MyCar.sol";
 
-contract MyCarTest is Test {
+contract MyCarTest is DSTest {
     MyCar public myCar;
 
     function setUp() public {
