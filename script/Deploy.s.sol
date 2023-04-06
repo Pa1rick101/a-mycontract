637d504b414309e3223bf3aa0f3be2102b916d86
Pa1rick101 <p.suehrer@gmail.com>
2023-04-06 16:58:25 +0200

    more testing of mint

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import {MyCar} from "src/MyCar.sol";
 
-contract CounterScript is Script {
+contract MyCarScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        new MyCar();
     }
+
+    function mint() public {}
 }
--- test/MyCar.t.sol
+++ test/MyCar.t.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "ds-test/test.sol";
+import "forge-std/Test.sol";
 import "../src/MyCar.sol";
 
-contract MyCarTest is DSTest {
+contract MyCarTest is Test {
     MyCar public myCar;
+    address testAddr = makeAddr("Test");
 
     function setUp() public {
         myCar = new MyCar();
@@ -14,5 +15,13 @@ contract MyCarTest is DSTest {
     function testExample() public {
         string memory name = myCar.name();
         assertEq(name, "MyCar");
+        emit log_named_string("The name of the NFT Collection is: ", name);
+    }
+
+    function testMintNFT() public {
+        myCar.safeMint(testAddr, "www.youtube.com");
+        uint256 balance = myCar.balanceOf(testAddr);
+        assertEq(balance, 1);
+        emit log_named_uint("The new address has minted ", balance);
     }
 }
