2e0f1ccd5f4e8396707ed8be177f4bc8a344e409
Pa1rick101 <p.suehrer@gmail.com>
2023-04-06 14:44:26 +0200

    testing setup done

--- test/MyCar.t.sol
+++ test/MyCar.t.sol
@@ -5,17 +5,14 @@ import "forge-std/Test.sol";
 import "../src/MyCar.sol";
 
 contract MyCarTest is Test {
-    //Counter public counter;
-    //function setUp() public {
-    //    counter = new Counter();
-    //    counter.setNumber(0);
-    //}
-    //function testIncrement() public {
-    //    counter.increment();
-    //    assertEq(counter.number(), 1);
-    //}
-    //function testSetNumber(uint256 x) public {
-    //    counter.setNumber(x);
-    //    assertEq(counter.number(), x);
-    //}
+    MyCar public myCar;
+
+    function setUp() public {
+        myCar = new MyCar();
+    }
+
+    function testExample() public {
+        string memory name = myCar.name();
+        assertEq(name, "MyCar");
+    }
 }
