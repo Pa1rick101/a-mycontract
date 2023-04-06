c42056750f9ea208f633f0897caf52df98e46300
Pa1rick101 <p.suehrer@gmail.com>
2023-04-06 10:09:58 +0200

    commented code for readability :)

--- src/MyCar.sol
+++ src/MyCar.sol
@@ -1,27 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+// Import necessary contracts from OpenZeppelin library
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+// Define the contract MyCar which inherits from ERC721, ERC721URIStorage, Pausable, and Ownable contracts
 contract MyCar is ERC721, ERC721URIStorage, Pausable, Ownable {
+    // Use the Counters library for generating unique tokenIds
     using Counters for Counters.Counter;
 
+    // Private variable for tracking tokenIds
     Counters.Counter private _tokenIdCounter;
 
+    // Constructor for MyCar contract
     constructor() ERC721("MyCar", "CAR") {}
 
+    // Function for pausing the contract, accessible only by the owner
     function pause() public onlyOwner {
         _pause();
     }
 
+    // Function for unpausing the contract, accessible only by the owner
     function unpause() public onlyOwner {
         _unpause();
     }
 
+    // Function for minting a new token, accessible only by the owner
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
@@ -29,6 +37,7 @@ contract MyCar is ERC721, ERC721URIStorage, Pausable, Ownable {
         _setTokenURI(tokenId, uri);
     }
 
+    // Function to be called before transferring a token
     function _beforeTokenTransfer(
         address from,
         address to,
@@ -40,12 +49,14 @@ contract MyCar is ERC721, ERC721URIStorage, Pausable, Ownable {
 
     // The following functions are overrides required by Solidity.
 
+    // Function for burning a token
     function _burn(
         uint256 tokenId
     ) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
+    // Function for getting the token URI
     function tokenURI(
         uint256 tokenId
     ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
